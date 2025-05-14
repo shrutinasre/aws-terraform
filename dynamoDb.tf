0487e649032b3a8671eae907a0d6c9d54de43982
shrutiJenkin <shruti.nasre10@gmail.com>
2025-05-14 16:34:08 +0530

    after deleting dynamo-state-block file

--- dynamoDb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "state-locking" {
-  hash_key = "LockID"
-  name = "dynamodb-state-locking"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
--- main.tf
+++ main.tf
@@ -112,3 +112,19 @@ terraform {
     use_lockfile = true
   }
 }
+
+
+
+
+
+
+#Resource dynamo db  for state locking
+# resource "aws_dynamodb_table" "state-locking" {
+#   hash_key = "LockID"
+#   name = "dynamodb-state-locking"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   billing_mode = "PAY_PER_REQUEST"
+# }
